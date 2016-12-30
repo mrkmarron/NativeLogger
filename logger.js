dec43886b11cc6281203b1ee82aed5860697fe95
Mark Marron <marron@microsoft.com>
2016-12-29 22:21:09 -0800

    Initial commit.

---
+++ logger.js
@@ -0,0 +1,7 @@
+//-------------------------------------------------------------------------------------------------------
+// Copyright (C) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+//-------------------------------------------------------------------------------------------------------
+
+//This code implements a node module that uses native runtime support to implement ultra-low overhea logging.
+
